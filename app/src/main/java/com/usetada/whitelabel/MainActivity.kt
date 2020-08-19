cc82dee321459ce62b9852d8044ee25cccd9eb86
yunus simulya <yunussimulya@gmail.com>
2020-08-19 16:51:58 +0700

    Setup Dummy named user

--- app/src/main/java/com/usetada/whitelabel/MainActivity.kt
+++ app/src/main/java/com/usetada/whitelabel/MainActivity.kt
@@ -2,10 +2,12 @@ package com.usetada.whitelabel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.urbanairship.UAirship
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        UAirship.shared().namedUser.id = "+6281100000000" //used after login, set to null when logout
     }
 }
